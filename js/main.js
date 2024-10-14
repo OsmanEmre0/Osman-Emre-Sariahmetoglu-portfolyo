22be31c4c7f630025c1191f2e5847e162af0632f
Osman Emre Sarıahmetoğlu <osariahnetoglu@gmail.com>
2024-10-15 02:18:49 +0300

    Contact Finish

--- js/main.js
+++ js/main.js
@@ -24,5 +24,35 @@ document.addEventListener('DOMContentLoaded', function () {
  });
 
 
+// Contact Control
+
+document.getElementById("phone").addEventListener("input", function(event) {
+
+    this.value = this.value.replace(/[^0-9]/g, '');
+
+
+    if (this.value.length > 11) {
+        this.value = this.value.slice(0, 11);
+    }
+});
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById("full-name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const budget = document.getElementById("budget").value;
+    const message = document.getElementById("message").value;
+
+    const body = `Full Name: ${fullName}\n \nEmail: ${email}\n \nPhone: ${phone}\n \nBudget: ${budget}\n \nMessage: ${message}`;
+    const mailtoLink = `mailto:osariahnetoglu@gmail.com?subject=${encodeURIComponent("")}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+});
+
+
+
+
 
 
